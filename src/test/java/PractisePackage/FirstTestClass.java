a4942c61bbc2974d024161b741a1330d8c8ef98e
Sridevi <Sridevi@192.168.32.1>
2022-11-22 12:48:01 +0530

    updated

--- src/test/java/PractisePackage/FirstTestClass.java
+++ src/test/java/PractisePackage/FirstTestClass.java
@@ -5,6 +5,7 @@ public class FirstTestClass {
 	public static void main(String[] args) {
 
 		System.out.println("First Prog in this Workspace");
+		System.out.println("Updated First Prog");
 	}
 
 }
